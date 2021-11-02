9a3a5342573ffeb9529f6a0818365b397d90e071
saravanan <saravanan1997@gmail.com>
2021-11-02 15:52:08 +0530

    newchange

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,20 +11,20 @@
     {{-- {{ config('app.name', 'Laravel') }} --}}
 
     <!-- Scripts -->
-    <script src="{{ asset('../js/app.js') }}" defer></script>
-    <script src="{{ asset('../js/link.js') }}" defer></script>
+    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="{{ asset('/js/link.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="{{ asset('../js/jquery.js') }}" defer></script>
+    <script src="{{ asset('/js/jquery.js') }}" defer></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
-    <script src="{{ asset('../js/cdn.js') }}" defer></script>
+    <script src="{{ asset('/js/cdn.js') }}" defer></script>
    
 
     <!-- Fonts -->
-    <link rel="stylesheet" href='{{asset("../css/crudtable.css")}}'>
-    <link rel="stylesheet" href='{{asset("../css/boot.css")}}'>
-    <link rel="stylesheet" href='{{asset("../css/confirm.css")}}'>
-    <link rel="stylesheet" href='{{asset("../css/normal.css")}}'>
+    <link rel="stylesheet" href='{{asset("/css/crudtable.css")}}'>
+    <link rel="stylesheet" href='{{asset("/css/boot.css")}}'>
+    <link rel="stylesheet" href='{{asset("/css/confirm.css")}}'>
+    <link rel="stylesheet" href='{{asset("/css/normal.css")}}'>
     {{-- <link rel="stylesheet" href="{{asset('../css/bgimg.css')}}"> --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -69,7 +69,6 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     WELCOME {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
